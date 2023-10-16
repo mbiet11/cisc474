8ea50d8ccd97d819a762d53074d7c727b5957e12
mbiet11 <mbietuko@udel.edu>
2023-10-16 02:49:38 -0400

    emialjs modifications

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,5 @@
-function send(){
-    (function(){
+(function(){
         emailjs.init("9O1vaHt6YRhMNVjGz");
-    })();
 
     document.addEventListener("DOMContentLoaded", function() {
         const contact = document.getElementById("contact");
@@ -11,36 +9,36 @@ function send(){
             errorMessages.textContent = "";
             let hasError = false;
 
-            var params = {
+            const params = {
             user_lname: document.getElementById("user_lname").value,
-             user_fname: document.getElementById("user_fname").value,
-             user_email: document.getElementById("user_email").value,
-             user_phone: document.getElementById("user_phone").value,
-             user_message: document.getElementById("user_message").value,
+            user_fname: document.getElementById("user_fname").value,
+            user_email: document.getElementById("user_email").value,
+            user_phone: document.getElementById("user_phone").value,
+            user_message: document.getElementById("user_message").value,
 
             };
 
-            if (user_lname.value.trim() === "") {
+            if (params.user_lname.trim() === "") {
                 hasError = true;
                 errorMessages.textContent += "Last Name is required. ";
             }
 
-            if (user_fname.value.trim() === "") {
+            if (params.user_fname.trim() === "") {
                 hasError = true;
                 errorMessages.textContent += "First Name is required. ";
             }
 
-            if (user_email.value.trim() === "" || !isValidEmail(user_email.value)) {
+            if (params.user_email.trim() === "" || !isValidEmail(user_email.value)) {
                 hasError = true;
                 errorMessages.textContent += "Invalid Email Address. ";
             }
 
-            if (user_phone.value.trim() === "" || !isValidPhone(user_phone.value)) {
+            if (params.user_phone.trim() === "" || !isValidPhone(user_phone.value)) {
                 hasError = true;
                 errorMessages.textContent += "Invalid Phone Number. ";
             }
 
-            if (user_message.value.trim() === "") {
+            if (params.user_message.trim() === "") {
                 hasError = true;
                 errorMessages.textContent += "Message is required. ";
             }
@@ -48,15 +46,11 @@ function send(){
             if (hasError) {
                 event.preventDefault();
             }
+            else {
+                sendEmail(params);
+            }
         });
  
-        var serviceID = "service_nz0n4v1";
-        var templateID = "template_qtmlbp6";
-
-        emailjs.send(serviceID, templateID, params )
-        .then (res => {alert("Email was sent!");
-        })
-        .catch();
 
         function isValidEmail(email) {
             const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -67,5 +61,17 @@ function send(){
             const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
             return phoneRegex.test(phone);
         }
+        function sendEmail(params) {
+            const serviceID = "service_nz0n4v1"; 
+            const templateID = "template_qtmlbp6"; 
+
+            emailjs.send(serviceID, templateID, params)
+                .then(res => {
+                    alert("Email was sent!");
+                })
+                .catch(error => {
+                    console.error("Email could not be sent:", error);
+                });
+        }
     });
-}
+})();
